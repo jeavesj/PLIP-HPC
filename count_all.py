5ec686f2bffeaaa1023a4abe4fd9715724a26762
Joelle Eaves <jeaves@msu.edu>
2026-02-02 14:51:21 -0500

    Modified to skip poses/sources that have already been completed (per the contents of the specified output csv)

--- count_all.py
+++ count_all.py
@@ -6,31 +6,43 @@ import subprocess
 from count_interactions import count_interactions
 
 sources = ['gnina', 'gnina-apo', 'gnina-af3', 'rosetta', 'crystal', 'boltz2']
-pdb_csv = '/mnt/research/woldring_lab/Members/Eaves/plip-plop/AF3/casf2016_smiles_seqs_cleaned.csv'
-base_dir = '/mnt/scratch/jeaves/plip'
-
 int_types = ['hydrophobic_interaction', 'hydrogen_bond', 'water_bridge', 'salt_bridge', 'pi_stack', 'pi_cation_interaction', 'halogen_bond']
 
+base_dir = '/mnt/scratch/jeaves/plip'
+
+pdb_csv = '/mnt/research/woldring_lab/Members/Eaves/plip-plop/AF3/casf2016_smiles_seqs_cleaned.csv'
+out_csv = '/mnt/research/woldring_lab/Members/Eaves/FAIR_PLBAP/results/plip_counts.csv'
+
 pdbids = pd.read_csv(pdb_csv)['pdb_id'].unique()
+done_df = pd.read_csv(out_csv)
 
 for source in sources:
     tar_path = os.path.join(base_dir, f'{source}_plips.tar.gz')
     extract_path = os.path.join(base_dir, source)
     
-    with tarfile.open(tar_path, 'r') as tar:
-        tar.extractall(path=extract_path, filter='data')
-    print(f'Extracted files for {source} to {extract_path})')
+    if not os.path.exists(extract_path):
+        with tarfile.open(tar_path, 'r') as tar:
+            tar.extractall(path=extract_path, filter='data')
+        print(f'Extracted files for {source} to {extract_path})')
+    
+    if source in done_df['source'].unique():
+        done_pdbs = done_df[done_df['source']==source]['pdbid'].unique()
 
     for pdbid in pdbids:
         pdbid = pdbid.lower()
+        
+        if pdbid in done_pdbs:
+            continue
         
         all_counts = []
         if source in ['crystal', 'boltz2']:
             count_dict = None
             pdb_dir = os.path.join(extract_path, pdbid)
+            if not os.path.exists(pdb_dir):
+                print(f'[WARN] Skipping {pdbid} {source} pose {poseid}. Directory does not exist: {pdb_dir}')
+                continue
             
             contents = os.listdir(pdb_dir)
-            
             for item in contents:
                 if str(item).endswith('.xml'):
                     count_dict = count_interactions(os.path.join(pdb_dir, item))
@@ -42,9 +54,10 @@ for source in sources:
                 poseid = str(n).zfill(4)
                 
                 pdb_dir = os.path.join(extract_path, f'{source}_best{poseid}', pdbid)
-            
+                if not os.path.exists(pdb_dir):
+                    print(f'[WARN] Skipping {pdbid} {source} pose {poseid}. Directory does not exist: {pdb_dir}')
+                    continue
                 contents = os.listdir(pdb_dir)
-                
                 for item in contents:
                     if str(item).endswith('.xml'):
                         count_dict = count_interactions(os.path.join(pdb_dir, item))
@@ -55,7 +68,7 @@ for source in sources:
                     all_counts.append({'pdbid': pdbid, 'source': source, 'pose': int(n), 'int_type': int_type, 'count': count_dict[int_type]})
         
         df = pd.DataFrame(all_counts)
-        df.to_csv('/mnt/research/woldring_lab/Members/Eaves/FAIR_PLBAP/results/plip_counts.csv', mode='a', index=False)
+        df.to_csv(out_csv, mode='a', index=False)
     
     subprocess.run(['rm', '-r', extract_path])
     print(f'Removed extracted files for {source} (path: {extract_path})')
